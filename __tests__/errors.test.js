ae294f1510b6c694bdf15697f619dcb607589afd
Grayson Gilmore <gilmoreg@live.com>
2017-05-25 00:04:05 -0500

    Test remaining error handlers

--- __tests__/errors.test.js
+++ __tests__/errors.test.js
@@ -1,58 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* globals describe, it, beforeEach, afterEach, expect, jest */
-// const express = require('express');
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-const { catchErrors } = require('../handlers/errors');
-/*
-const app = express();
+const express = require('express');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const errorHandlers = require('../handlers/errors');
+const httpMocks = require('node-mocks-http');
+
 chai.use(chaiHttp);
-
-// Middleware to throw errors
-app.get('/error', (req, res, next) => {
-  next(Error('error'));
-});
-
-app.use(errorHandlers.notFound);
-app.use(errorHandlers.developmentErrors);
-app.use(errorHandlers.productionErrors);
-
+let app;
 let server;
 
-describe('Error Handlers', () => {
-  beforeEach(() => {
-    server = app.listen(5000);
-  });
-  afterEach(() => {
-    server.close();
-  });
-
-  it('should respond with not found', (done) => {
-    chai.request(app)
-      .get('/doesnotexist')
-      .end((err, res) => {
-        expect(res.status).toEqual(404);
-        done();
-      });
-  });
-
-  it('should respond with production and development errors', (done) => {
-    chai.request(app)
-      .get('/error')
-      .end((err, res) => {
-        expect(res.status).toEqual(500);
-        done();
-      });
-  });
-}); */
-
-describe('Error handler middlewares', () => {
+describe('catchErrors Handler', () => {
   it('catchErrors middleware catches a Promise.reject', (done) => {
     const req = {};
     const res = {};
 
     const route = () => Promise.reject('testing error');
-    const routeWithCatcher = catchErrors(route);
+    const routeWithCatcher = errorHandlers.catchErrors(route);
     const next = (err) => {
       expect(err).toEqual('testing error');
       done();
@@ -65,9 +29,64 @@ describe('Error handler middlewares', () => {
     const res = {};
 
     const route = () => Promise.resolve('testing success');
-    const routeWithCatcher = catchErrors(route);
+    const routeWithCatcher = errorHandlers.catchErrors(route);
     const next = jest.fn();
     routeWithCatcher(req, res, next);
     expect(next.mock.calls.length).toBe(0);
+  });
+});
+
+describe('Error Reporting Middleware', () => {
+  beforeEach(() => {
+    app = express();
+  });
+  afterEach(() => {
+    server.close();
+  });
+
+  it('should respond with not found', (done) => {
+    app.use(errorHandlers.notFound);
+    server = app.listen(5000);
+
+    chai.request(app)
+      .get('/doesnotexist')
+      .end((err, res) => {
+        expect(res.status).toEqual(404);
+        done();
+      });
+  });
+
+  it('should respond with development errors', () => {
+    const err = {
+      stack: 'test-stack',
+      message: 'test-error',
+      status: 500,
+    };
+    const req = httpMocks.createRequest();
+    const res = httpMocks.createResponse();
+    res.status = jest.fn();
+    res.json = jest.fn();
+    errorHandlers.developmentErrors(err, req, res);
+    expect(res.status.mock.calls[0][0]).toBe(500);
+    const json = res.json.mock.calls[0][0];
+    expect(json.message).toBe('test-error');
+    expect(json.status).toBe(500);
+    expect(json.stackHighlighted).toBe('test-stack');
+  });
+
+  it('should respond with production errors', () => {
+    const err = {
+      stack: 'test-stack',
+      message: 'test-error',
+      status: 500,
+    };
+    const req = httpMocks.createRequest();
+    const res = httpMocks.createResponse();
+    res.status = jest.fn();
+    res.json = jest.fn();
+    errorHandlers.productionErrors(err, req, res);
+    expect(res.status.mock.calls[0][0]).toBe(500);
+    const json = res.json.mock.calls[0][0];
+    expect(json.message).toBe('test-error');
   });
 });
